dc618b27ba884b9cd15c4d952a5f0563435b63cd
Sebastian Balling <sebastian.balling@jumo.net>
2023-11-15 11:52:15 +0100

    add files for testing, linting and prettifier

---
+++ test/browseTest.js
@@ -0,0 +1,14 @@
+import { getProviderList } from '../src/browseProcessValues.js'
+import { assert } from 'chai';
+
+describe('Browsing', () => {
+  describe('browseProcessValues.js', () => {
+    it('getProviderList is of type function', () => {
+      assert.isFunction(getProviderList);
+    });
+    it('returns an answer', () => {
+
+      assert.isFunction(getProviderList);
+    });
+  });
+});
